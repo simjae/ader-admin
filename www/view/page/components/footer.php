eea55f5b368065a2483d76cc15b055719fc1f376
simjae <sjhwo9370@gmail.com>
2022-09-23 12:15:53 +0900

    데스크탑 footer 구성

--- www/view/page/components/footer.php
+++ www/view/page/components/footer.php
@@ -1,4 +1,23 @@
-<ul class="">
+<style>
+    .f_desk {
+        color: #fff;
+        display: grid;
+        grid-template-columns: repeat(16, 120px);
+        gap: 10px;
+        max-width: 1920px;
+        margin: 0 auto;
+    }
+    
+    .f__title {
+        padding-top: 10px;
+    }
+    .f_content {
+        padding-top: 10px;
+    }
+</style>
+
+<!-- 모바일 -->
+<ul class="lg:hidden">
     <li class="about drop__menu">
         <div class="drop__down__wrap">
             <span>About ADERERROR</span><img src="/images/nav/plus.svg" alt="">
@@ -70,3 +89,78 @@
         </div>
     </li>
 </ul>
+<!-- 데스크탑 -->
+<div class="hidden lg:block">
+    <ul class="f_desk">
+        <li class="about" style="grid-column: 1/4;">
+            <div class="f__title">
+                <span class="p-0">About ADERERROR</span class="p-0">
+            </div>
+            <div class="f_content">
+                <p class="about">ADERERROR (아더에러)는 2014년 설립되었으며 패션을 기반으로 한 문화 커뮤니케이션 브랜드입니다. ADERERROR는 ‘but near missed things’ 이라는 브랜드 슬로건, 철학을 바탕으로 사람들이 일상에서 쉽게 놓치고 있는 것들을 익숙하지만 낯설고, 새롭게 느낄 수 있도록 표현하는 활동에 집중하고 있으며, 사진, 영상, 공간, 디자인, 예술, 가구 등 문화 콘텐츠를 우리의 방식으로 재편집하여 새로운 문화를 제안합니다. ADER는 모든 영역 간의 커뮤니케이션 디자인하는 것을 브랜드 핵심 가치로서 추구합니다.</p>
+            </div>
+        </li>
+        <li class="footer__content" style="grid-column: 8/10;">
+            <div class="f__title">
+                <span class="p-0">법적 고지사항</span class="p-0">
+            </div>
+            <div class="f_content">
+                <ul>
+                    <li class="disclaimer">온라인 스토어 이용가이드</li>
+                    <li class="disclaimer">이용 약관</li>
+                    <li class="disclaimer">개인정보 처리방침</li>
+                </ul>
+            </div>
+        </li>
+        <li class="footer__content" style="grid-column: 10/11;">
+            <div class="f__title">
+                <span class="p-0">소셜 미디어</span class="p-0">
+            </div>
+            <div class="f_content">
+                <div class="social">
+                    <div class="flex ">
+                        <img class="p-0" src="/images/footer/wechat.svg" alt="">
+                        <img class="p-2" src="/images/footer/facebook.svg" alt="">
+                        <img class="p-2" src="/images/footer/instagram.svg" alt="">
+                        <img class="p-2" src="/images/footer/youtube.svg" alt="">
+                    </div>
+                    <div class="flex ">
+                        <img class="p-0" src="/images/footer/kakao-ch.svg" alt="">
+                        <img class="p-2" src="/images/footer/pinterest.svg" alt="">
+                        <img class="p-2" src="/images/footer/vimeo.svg" alt="">
+                        <img class="p-2" src="/images/footer/weibo.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </li>
+        <li class="footer__content" style="grid-column: 12/14;">
+            <div class="f__title">
+                <span class="p-0">고객센터</span class="p-0">
+            </div>
+            <div class="f_content">
+                <p class="service">ADER 3F 53, Yeonmujang-gil,</p>
+                <p class="service">Seongdong-gu, Seoul, Korea</p>
+                <p class="service">TEL. 02-792-2232</p>
+                <p class="service">Office hour Mon - Fri AM 10:00 - PM 5:00</p>
+            </div>
+        </li>
+        <li class="footer__content" style="grid-column: 14/17;">
+            <div class="f__title">
+                <span class="p-0">회사정보</span class="p-0">
+            </div>
+            <div class="f_content">
+                <p class="info">Company | ADER</p>
+                <p class="info">Business Name | FIVE SPACE CO.,LTD</p>
+                <p class="info">Business License | 760-87-01757</p>
+                <p class="info">Mail-order License No. | 제 2021-서울성동-01588호</p>
+                <p class="info">CEO | HANN </p>
+                <p class="info">OFFICE | ADER 3F 53, Yeonmujang-gil, Seongdong-gu, Seoul, Korea</p>
+            </div>
+        </li>
+    </ul>
+    <ul class="last">
+        <div class="f__title">
+            <span class="p-0">© ADERERROR 2022</span class="p-0">
+        </div>
+    </ul>
+</div>
